d671ca64edb588fd8cc18648ebff11c6eb65481c
Walter Gordy <walter.gordy@gmail.com>
2018-08-13 00:40:19 -0600

    Removes unnecessary usings;

--- src/Neat Implemtation/ConnectionGene.cs
+++ src/Neat Implemtation/ConnectionGene.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Neat_Implemtation
-{
+namespace Neat_Implemtation {
     class ConnectionGene
     {
         int inNode;
