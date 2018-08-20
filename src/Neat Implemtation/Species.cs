1eb4e09a01095046f00bbb731933fb7f34552b5a
Walter Gordy <walter.gordy@gmail.com>
2018-08-19 18:59:11 -0600

    Adds species class

---
+++ src/Neat Implemtation/Species.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neat_Implemtation {
+    class Species {
+        Genome ApexGenome;
+        List<Genome> population = new List<Genome>();
+
+        public Species() { }
+
+        public Species(Genome g) {
+            ApexGenome = g;
+            population.Add(g);
+        }
+
+
+    }
+}
