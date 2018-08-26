eb84d76ec0aad8732d21142b926ce457b2642e1c
Walter Gordy <walter.gordy@gmail.com>
2018-08-25 21:13:45 -0600

    Add mutate connection to Genome

--- src/BasicNeuralNetwork/Genome.cs
+++ src/BasicNeuralNetwork/Genome.cs
@@ -153,6 +153,17 @@ namespace BasicNeuralNetwork
         }
 
         /// <summary>
+        /// Connection weights mutate as in any NE system, with each connection either perturbed or not at each generation.
+        /// </summary>
+        public void mutateConnectionWeights() {
+            foreach (KeyValuePair<int, ConnectionGene> g in Connections) {
+                if(r.NextDouble() > 0.5)
+                    g.Value.Weight += (float)(2 * r.NextDouble() - 1);
+                }
+            }
+        }
+
+        /// <summary>
         /// In the add connection mutation, a single new connection gene with a 
         /// random weight is added connecting two previously unconnected nodes.
         /// </summary>
