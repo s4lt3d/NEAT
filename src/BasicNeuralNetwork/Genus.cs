5a5b9a855f2c1a9e597110e05ee4ce71fa850919
Walter Gordy <walter.gordy@gmail.com>
2018-08-25 23:57:53 -0600

    Adds genus and settings

---
+++ src/BasicNeuralNetwork/Genus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEATNeuralNetwork
+{
+    class Genus
+    {
+        List<Species> Species = new List<Species>();
+        int generationNumber = 0;
+
+        public static Genus instance = null;
+
+        public Genus() {
+            instance = this;
+        }
+    }
+}
--- src/BasicNeuralNetwork/NEATSettings.cs
+++ src/BasicNeuralNetwork/NEATSettings.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace NEATNeuralNetwork {
     public class NEATSettings {
-        public static float CompatibilityThreshold = 1;
+        public static double CompatibilityThreshold = 1;
+        public static double DeltaDisjoint = 2;
+        public static double DeltaWeights = 0.4;
+        public static double DeltaThreshold = 1;
+
+        public static double WeightMutation = 0.25;
+        public static double ConnectionMutation = 2;
+        public static double NodeMutation = 0.5;
+        public static double EnableMutation = 0.2;
+        public static double DisableMutation = 0.4;
+        public static double Perturbation = 0.9;
+        public static double Crossover = 0.75;
 
     }
 }
